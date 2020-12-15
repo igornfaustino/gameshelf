0f78523c3aae6c2b86bf108040cf59bc30c1be86
igornfaustino <igor@nfaustino.com>
2020-12-15 13:28:01 -0300

    [setup] add eslint and prettier

--- app/components/elements/Footer.tsx
+++ app/components/elements/Footer.tsx
@@ -1,10 +1,10 @@
-import { useTranslation, Trans } from "../../config/i18next";
+import { useTranslation, Trans } from '../../config/i18next';
 
 const Footer = () => {
-  const { t } = useTranslation("footer");
+  const { t } = useTranslation('footer');
   return (
     <footer>
-      {t("copyright")}
+      {t('copyright')}
       <div>
         <Trans i18nKey="footer:icons">
           Icons made by
@@ -12,6 +12,7 @@ const Footer = () => {
             href="https://www.flaticon.com/authors/freepik"
             title="Freepik"
             target="_blank"
+            rel="noreferrer"
           >
             Freepik
           </a>
@@ -19,6 +20,7 @@ const Footer = () => {
             href="https://www.flaticon.com/authors/pixel-perfect"
             title="Pixel perfect"
             target="_blank"
+            rel="noreferrer"
           >
             Pixel perfect
           </a>
@@ -26,10 +28,11 @@ const Footer = () => {
             href="https://www.flaticon.com/authors/smashicons"
             title="Smashicons"
             target="_blank"
+            rel="noreferrer"
           >
             Smashicons
           </a>
-          <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">
+          <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noreferrer">
             www.flaticon.com
           </a>
         </Trans>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../app/styles/globals.css";
-import { appWithTranslation } from "../app/config/i18next";
+import '../app/styles/globals.css';
+import { appWithTranslation } from '../app/config/i18next';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
